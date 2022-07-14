863166481d98872bb89ffde0a94cb05279f16734
Aristidis Gianicopulos loguercio <agianico@c1r7s1.42madrid.com>
2022-07-14 19:48:53 +0200

    implemented cd functionality, error management and start doing the pipes. exam_rank_04

--- exam_rank_04/microshell.c
+++ exam_rank_04/microshell.c
@@ -72,54 +72,94 @@ void	parse_arg(t_args *mini)
 			mini->is_pipe = 1;
 		else if (strcmp(";", mini->args[i]) == 0)
 			mini->is_semi_colon = 1;
+		if (mini->is_pipe == 1 && mini->is_semi_colon == 1)
+			return ;
 		i++;
 	}
 	//printf("is_pipe: %d\nis_semi_colon: %d\n", mini->is_pipe, mini->is_semi_colon);
 }
 
-void	exec_cmds(t_args *mini, char **env)
+int		show_error(char const *str)
+{
+	if (str)
+		write(2, str, ft_strlen(str));
+	return (EXIT_FAILURE);
+}
+
+int		exec_complex_cmds(t_args *mini, char **env, int track)
+{
+	pid_t	pid;
+	int		ret;
+
+	ret = 0;
+	pid = fork();
+	if (pid < 0)
+		return (show_error("error: fatal\n"));
+	else if (pid == 0)
+	{
+		if ((ret = execve(mini->args[track], mini->args, env)) < 0)
+		{
+			show_error("error: cannot execute: ");
+			show_error(mini->args[track]);
+			show_error("\n");
+		}
+		exit(ret);
+	}
+	return (ret);
+}
+
+int		exec_cmds(t_args *mini, char **env)
 {
 	int	i;
+	int	ret;
 
 	i = 0;
+	ret = EXIT_SUCCESS;
 	while (mini->args[i])
 	{
 		if (strcmp("cd", mini->args[i]) == 0)
 		{
+			ret = EXIT_SUCCESS;
 			if (chdir(mini->args[i + 1]))
 			{
 				if (!mini->args[i + 1])
-					printf("error: cd: bad arguments\n");
+					ret = show_error("error: cd: bad arguments\n");
 				else
 				{
-					printf("error: cd: cannot change directory to ");
-					printf("%s\n", mini->args[i + 1]);
+					ret = show_error("error: cd: cannot change directory to ");
+					show_error(mini->args[i + 1]);
+					show_error("\n");
+					i++;
 				}
-				return;
 			}
 		}
-		else if (execve(mini->args[i], mini->args, env) < 0)
-			return;
+		else
+			ret = exec_complex_cmds(mini, env, i);
+		// else if (execve(mini->args[i], mini->args, env) < 0)
+		// 	return -1;
 		i++;
 	}
+	return (ret);
 }
 
 int main(int argc, char **argv, char **env)
 {
-	(void)env;
 	t_args	*mini;
 	int		i;
+	int		ret;
 
-	i = 1;
+	ret = EXIT_SUCCESS;
 	mini = ft_init();
 	mini->args = (char **)malloc(sizeof(char *) * (argc));
 	argv_to_struct(mini, argv);
 	parse_arg(mini);
-	exec_cmds(mini, env);
-	// i = 0;
+	if (mini->args)
+		ret = exec_cmds(mini, env);
+	i = 0;
 	// while (mini->args[i] != '\0')
 	// {
 	// 	printf("cadena: %s\n", mini->args[i]);
 	// 	i++;
 	// }
+	return (ret);
 }
